00e65b4326479a7e556657e5dac7551de9f5ff4c
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-01-12 22:36:32 +0700

    .....

--- thecourse_be/routes/api.php
+++ thecourse_be/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::controller(UserController::class)->group(function () {
+    Route::get('/getData', 'getData');
 });
